b2bdc4027694fb073de0b761e2ee73856ee62e17
Osmarcheeto <osmar.chavez@icloud.com>
2025-12-10 13:23:21 -0600

    Update 2_while_loops.py

--- 2_while_loops.py
+++ 2_while_loops.py
@@ -1,6 +1,20 @@
+
+Benjamin Barrera, Now
 # Given:
 colors = ["red", "blue", "green", "yellow", "purple"]
 
 # Challenge:
 # Use a while loop to print each color UNTIL you find "yellow".
 # Do NOT print "yellow" — stop before it.
+
+index = 0
+while index < len(colors):
+    if colors[index] == "yellow":
+        break
+    print(colors[index])
+    index += 1
+    # index += 1 is shortned for index = index + 1
+#expalanation: 
+# We start with index 0 (first color).
+#while index is less than length of colors list (5):
+#check if color at current index is "yellow":
